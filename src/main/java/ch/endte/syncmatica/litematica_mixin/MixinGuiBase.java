7cfaf791c2f1fe6e464a83e9faedd595906a9ac9
nnnik <nnnik@web.de>
2021-09-15 16:37:28 +0200

    Add final

--- src/main/java/ch/endte/syncmatica/litematica_mixin/MixinGuiBase.java
+++ src/main/java/ch/endte/syncmatica/litematica_mixin/MixinGuiBase.java
@@ -3,6 +3,7 @@ package ch.endte.syncmatica.litematica_mixin;
 import ch.endte.syncmatica.litematica.ScreenHelper;
 import fi.dy.masa.malilib.gui.GuiBase;
 import fi.dy.masa.malilib.gui.button.ButtonBase;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +15,7 @@ import java.util.List;
 @Mixin(GuiBase.class)
 public abstract class MixinGuiBase {
 
+    @Final
     @Shadow(remap = false)
     private List<ButtonBase> buttons;
 
