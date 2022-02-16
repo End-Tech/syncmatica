fdf9ecda163f3d2fe9835310b81a8fd7aefc7f1d
nnnik <nnnik@web.de>
2022-02-16 17:11:43 +0100

    Refactor one class

--- src/main/java/ch/endte/syncmatica/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/ch/endte/syncmatica/mixin/MixinClientPlayNetworkHandler.java
@@ -1,24 +1,13 @@
 package ch.endte.syncmatica.mixin;
 
-import ch.endte.syncmatica.IFileStorage;
-import ch.endte.syncmatica.RedirectFileStorage;
-import ch.endte.syncmatica.SyncmaticManager;
-import ch.endte.syncmatica.Syncmatica;
-import ch.endte.syncmatica.communication.ClientCommunicationManager;
-import ch.endte.syncmatica.communication.CommunicationManager;
-import ch.endte.syncmatica.communication.ExchangeTarget;
-import ch.endte.syncmatica.litematica.LitematicManager;
-import ch.endte.syncmatica.litematica.ScreenHelper;
+import ch.endte.syncmatica.mixin_actor.ActorClientPlayNetworkHandler;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,22 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class MixinClientPlayNetworkHandler {
-
-    @Unique
-    private ExchangeTarget exTarget = null;
-    @Unique
-    private CommunicationManager clientCommunication;
-
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void startupClient(final MinecraftClient client, final Screen screen, final ClientConnection connection, final GameProfile profile, final CallbackInfo ci) {
-        final IFileStorage data = new RedirectFileStorage();
-        final SyncmaticManager man = new SyncmaticManager();
-        exTarget = new ExchangeTarget((ClientPlayNetworkHandler) (Object) this);
-        final CommunicationManager comms = new ClientCommunicationManager(exTarget);
-        Syncmatica.initClient(comms, data, man);
-        clientCommunication = comms;
-        ScreenHelper.init();
-        LitematicManager.getInstance().setActiveContext(Syncmatica.getContext(Syncmatica.CLIENT_CONTEXT));
+    private void startupEvent(final MinecraftClient client, final Screen screen, final ClientConnection connection, final GameProfile profile, final CallbackInfo ci) {
+        ActorClientPlayNetworkHandler.getInstance().startEvent((ClientPlayNetworkHandler) (Object) this);
     }
 
     @Inject(method = "onCustomPayload", at = @At("HEAD"), cancellable = true)
@@ -49,11 +25,6 @@ public abstract class MixinClientPlayNetworkHandler {
         if (!MinecraftClient.getInstance().isOnThread()) {
             return; //only execute packet on main thread
         }
-        final Identifier id = packet.getChannel();
-        final PacketByteBuf buf = packet.getData();
-        if (clientCommunication.handlePacket(id)) {
-            clientCommunication.onPacket(exTarget, id, buf);
-            ci.cancel(); // prevent further unnecessary comparisons and reporting a warning
-        }
+        ActorClientPlayNetworkHandler.getInstance().packetEvent(packet, ci);
     }
 }
