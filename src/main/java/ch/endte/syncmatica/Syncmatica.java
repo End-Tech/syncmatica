3002e0367a0b81f73482264769fd6aed7f216780
kpzip <kpzipgames@gmail.com>
2023-06-12 13:02:35 -0700

    Version Bump

--- src/main/java/ch/endte/syncmatica/Syncmatica.java
+++ src/main/java/ch/endte/syncmatica/Syncmatica.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 public class Syncmatica {
 
-    public static final String VERSION = "0.3.8";
+    public static final String VERSION = "0.3.9";
     public static final String MOD_ID = "syncmatica";
 
     private static final String SERVER_PATH = "." + File.separator + "syncmatics";
